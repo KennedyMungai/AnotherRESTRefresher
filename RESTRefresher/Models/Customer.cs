1817a8dea2f72a78b6b774f7c7e3e73bdfb58c81
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-21 13:13:16 +0300

    Made the Id a string

--- RESTRefresher/Models/Customer.cs
+++ RESTRefresher/Models/Customer.cs
@@ -2,7 +2,7 @@ namespace RESTRefresher.Models;
 
 public class Customer
 {
-    public int Id { get; set; }
+    public string? Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 }
