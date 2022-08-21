1df3cead578a5ba4a542b607fe83a707e99d3269
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-21 13:11:31 +0300

    Created a blank model class for Customer

---
+++ RESTRefresher/Models/Customer.cs
@@ -0,0 +1,8 @@
+namespace RESTRefresher.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
