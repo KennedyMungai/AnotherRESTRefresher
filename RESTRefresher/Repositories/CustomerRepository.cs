c9c768d756a9e073ddb3ed018288d6df94581705
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-21 13:24:18 +0300

    Imported the Models namespace

--- RESTRefresher/Repositories/CustomerRepository.cs
+++ RESTRefresher/Repositories/CustomerRepository.cs
@@ -1,9 +1,15 @@
+using System.Collections.Concurrent;
+using RESTRefresher.Data;
 using RESTRefresher.Models;
 
 namespace RESTRefresher.Repositories;
 
 public class CustomerRepository : ICustomerRepository
 {
+    private static ConcurrentDictionary<string, Customer> customersCache;
+
+    private ApplicationDbContext db;
+
     public Task<Customer?> CreateAsync(Customer c)
     {
         throw new NotImplementedException();
