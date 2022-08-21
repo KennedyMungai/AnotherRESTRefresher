63931b953698e410b7e068e66d57654678f01c64
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-21 13:44:44 +0300

    Created a skeleton for the customers controller

---
+++ RESTRefresher/Controllers/CustomersController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTRefresher.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController : ControllerBase
+{
+    
+}
